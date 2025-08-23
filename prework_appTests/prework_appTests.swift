98b197532b341db13969426a2c97ab0f9b4fe646
p sriman reddy <pr.sriman@gmail.com>
2025-08-22 22:23:27 -0400

    Initial Commit

---
+++ prework_appTests/prework_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  prework_appTests.swift
+//  prework_appTests
+//
+//  Created by sriman on 8/22/25.
+//
+
+import Testing
+@testable import prework_app
+
+struct prework_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ prework_appUITests/prework_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  prework_appUITestsLaunchTests.swift
+//  prework_appUITests
+//
+//  Created by sriman on 8/22/25.
+//
+
+import XCTest
+
+final class prework_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
